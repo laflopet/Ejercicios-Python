c9fcdb1fa39d60d62dad25bc5d9e7e0ce87fbc31
laflopet <110005453+laflopet@users.noreply.github.com>
2022-10-25 20:06:33 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ estatura = float(input("Escribe tu estatura en Metros aqui: "))
 
 IMC = kilogramos / estatura ** 2
 
-print("Tu Indice de Masa Corporal es de: ", IMC)
+print("Tu Indice de Masa Corporal es de: ", round(IMC, 2))
