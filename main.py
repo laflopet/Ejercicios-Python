a611ca5bf4a5edc9ad6287a851e2cd2a8856d473
laflopet <laflopet@gmail.com>
2022-10-25 20:02:30 -0500

    Ejercicios de Python

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+kilogramos = int(input("Escribe tu peso en KG aqui: "))
+estatura = float(input("Escribe tu estatura en Metros aqui: "))
+
+IMC = kilogramos / estatura ** 2
+
+print("Tu Indice de Masa Corporal es de: ", IMC)
